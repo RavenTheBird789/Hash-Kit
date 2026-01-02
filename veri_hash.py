e422b79f850f1584f04c94bc484ea87bf972f215
Art3mis_XP <northwalker676@gmail.com>
2026-01-02 14:29:34 -0500

    Update veri_hash.py

--- veri_hash.py
+++ veri_hash.py
@@ -1,8 +1,15 @@
 # VeriHash: A simple hash comparison tool
 
-hash1 = input("Enter first hash: ")
-hash2 = input("Enter second hash: ")
-if hash1 == hash2:
-    print("The hashes match!")
-else:
-    print("The hashes do not match.")
+def red(text: str) -> str:
+    return f"\033[91m{text}\033[0m"
+
+def green(text: str) -> str:
+    return f"\033[92m{text}\033[0m"
+
+def vh():
+    hash1 = input("Enter first hash: ")
+    hash2 = input("Enter second hash: ")
+    if hash1 == hash2:
+        print(green("The hashes match!")) # Output if hashes match
+    else:
+        print(red("The hashes do not match.")) # Output if hashes do not match
